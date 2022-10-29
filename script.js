7c79a085cdb51fa6922de54f3e494e61b90df1e2
EC2 Default User <ec2-user@ip-172-31-9-195.ap-northeast-1.compute.internal>
2022-10-29 09:53:01 +0000

    Ajax-OpenWether

---
+++ script.js
@@ -0,0 +1,32 @@
+// （API_KEY には、"取得したAPIキー" を記述）
+const API_KEY = "9c2f1eb999920cd42d0c9547ea2d3a7f";
+
+$(function(){
+  $('#btn').on('click',function(){
+     // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      //通信成功
+        // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      //通信失敗
+        alert('通信に失敗しました。');  
+    })
+  });
+});
